904c5b875f44f11c7d7f254c919d060e5823b361
binhangyuan <yuanbinhang@hotmail.com>
2023-01-09 14:24:14 +0100

    debug slurm

--- start_alpa_bloom.sh
+++ start_alpa_bloom.sh
@@ -1,1 +1,11 @@
-python serving_dist_alpa_nlp_model.py --together_model_name together-alpa-bloom --alpa_model_name bloom --model_path /home/fm/helm_models/alpa_models/
+# A hard code example
+
+cd /home/fm/dev/Quick_Deployment_HELM
+
+# conda activate alpa
+
+/opt/conda/envs/alpa/bin/ray start --head
+
+./together start &
+
+/opt/conda/envs/alpa/bin/python serving_dist_alpa_nlp_model.py --together_model_name together-alpa-bloom --alpa_model_name bloom --model_path /home/fm/helm_models/alpa_models/
