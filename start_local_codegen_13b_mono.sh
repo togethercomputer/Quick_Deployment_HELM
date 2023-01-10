1000ca5f8de0b3b4088d38a73df473da8119f74e
binhangyuan <yuanbinhang@hotmail.com>
2023-01-10 23:49:51 +0100

    debug

--- start_local_codegen_13b_mono.sh
+++ start_local_codegen_13b_mono.sh
@@ -1,3 +1,5 @@
 cd /home/fm/dev/Quick_Deployment_HELM
 
+./together start &
+
 /opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/codegen-16B-mono --hf_model_name Salesforce/codegen-16B-mono --model_path /home/fm/helm_models/hf_models/codegen-16B-mono
