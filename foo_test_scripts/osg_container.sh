e881dab4401b9a468806bb622a1b8bc5a70a5a72
binhangyuan <yuanbinhang@hotmail.com>
2023-01-12 09:30:18 +0100

    debug condor

---
+++ foo_test_scripts/osg_container.sh
@@ -0,0 +1,8 @@
+apptainer shell \
+            --home $PWD:/srv \
+            --pwd /srv \
+            --bind /cvmfs \
+            --scratch /var/tmp \
+            --scratch /tmp \
+            --contain --ipc --pid \
+            /cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-ubuntu-20.04:latest/
