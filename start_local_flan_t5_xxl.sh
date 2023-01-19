861d34f9ed5d34c011dfa132aa9a2f3acf287925
binhangyuan <yuanbinhang@hotmail.com>
2023-01-19 10:25:20 +0100

    add flan-t5-xxl

--- start_local_flan_t5_xxl.sh
+++ start_local_flan_t5_xxl.sh
@@ -1,5 +1,7 @@
 cd /home/fm/dev/Quick_Deployment_HELM
 
+
 ./together start &
+
 
 /opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/flan-t5-xxl --hf_model_name google/flan-t5-xxl --model_path /home/fm/helm_models/hf_models/flan-t5-xxl
