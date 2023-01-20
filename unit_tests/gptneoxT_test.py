1593a505ca11a60748470b22673e06cc34d59e1a
binhangyuan <yuanbinhang@hotmail.com>
2023-01-20 07:58:34 +0100

    add added latest gptneoxT

---
+++ unit_tests/gptneoxT_test.py
@@ -0,0 +1,21 @@
+import requests
+
+prompt = "Where is Zurich?"
+top_p = 1.0
+temperature = 0.5
+max_new_tokens = 5
+
+
+my_post_dict = {
+    "model": "together/gpt-neoxT-20B-soda-v0.1",
+    "prompt": prompt,
+    "top_p": float(top_p),
+    "temperature": float(temperature),
+    "max_tokens": int(max_new_tokens),
+    "stop": [],
+    "logprobs": 3
+}
+
+print(my_post_dict)
+response = requests.get("https://staging.together.xyz/api/inference", params=my_post_dict).json()
+print(response)
