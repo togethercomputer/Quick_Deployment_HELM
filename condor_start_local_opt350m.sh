b8e30c21076f3bfed27a18d90256ddb8848b882e
binhangyuan <yuanbinhang@hotmail.com>
2023-01-31 21:42:18 +0100

    debug osg

--- condor_start_local_opt350m.sh
+++ condor_start_local_opt350m.sh
@@ -12,4 +12,4 @@ chmod 777 together
 
 ./together start &
 
-./alpa/bin/python serving_local_nlp_model.py --together_model_name together/opt-350b --hf_model_name facebook/opt-350m
+./alpa/bin/python serving_local_nlp_model.py --together_model_name together/opt-350m --hf_model_name facebook/opt-350m
