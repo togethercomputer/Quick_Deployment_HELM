d7cf51ab9a997c403f5fc71da4680de94e9c5e89
binhangyuan <yuanbinhang@hotmail.com>
2023-01-31 17:14:42 +0100

    debug osg

--- condor_start_local_opt350m.sh
+++ condor_start_local_opt350m.sh
@@ -10,7 +10,7 @@ echo "files in /opt/conda/envs/:"
 ls /opt/conda/envs/
 
 
-# ./together start &
+./together start &
 
 /opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/opt-350b --hf_model_name facebook/opt-350m
 
