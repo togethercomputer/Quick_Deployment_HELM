333cf9029e19e1374f9d2996cbc4073ddedfe849
binhangyuan <yuanbinhang@hotmail.com>
2023-01-31 16:31:10 +0100

    debug osg

--- condor_start_local_opt350m.sh
+++ condor_start_local_opt350m.sh
@@ -6,9 +6,13 @@ pwd
 echo "files in current directory:"
 ls
 
+echo "files in /opt/conda/envs/:"
+ls /opt/conda/envs/
+
+
 # ./together start &
 
-/etc/singularity run --nv --bind ./:/home/fm alpa_general.sif /opt/conda/envs/alpa/bin/python /home/fm/serving_local_nlp_model.py --together_model_name together/opt-350b --hf_model_name facebook/opt-350m
+/opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/opt-350b --hf_model_name facebook/opt-350m
 
 
 #nvidia-docker run --rm --gpus '"device=0"' --ipc=host -v ./:/home/fm binhang/alpa /opt/conda/envs/alpa/bin/python /home/fm/serving_local_nlp_model.py --together_model_name together/opt-350b --hf_model_name facebook/opt-350m
