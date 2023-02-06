99501ff1bc433d7944e62084f762c838c3098c21
binhangyuan <yuanbinhang@hotmail.com>
2023-02-06 16:08:20 +0100

    debug osg container

---
+++ condor_singularity_start_local_opt350m.sh
@@ -0,0 +1,16 @@
+nvidia-smi
+
+echo "current directory:"
+pwd
+
+echo "files in current directory:"
+ls
+
+echo "files in /opt/conda/envs directory:"
+ls /opt/conda/envs
+
+chmod 777 together
+
+./together start &
+
+/opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/opt-350m --hf_model_name facebook/opt-350m
