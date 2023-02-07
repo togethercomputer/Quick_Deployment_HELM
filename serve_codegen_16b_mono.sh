4e4ed7fc5157320cdd7b93aee7bee5bfd7ae22f1
Xiaozhe Yao <askxzyao@gmail.com>
2023-02-07 15:14:05 +0100

    fix bugs

---
+++ serve_codegen_16b_mono.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/codegen-16b-mono --hf_model_name Salesforce/codegen-16B-mono --model_path /home/user/.together/models/codegen-16b-mono
