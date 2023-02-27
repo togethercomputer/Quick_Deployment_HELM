82552dc645945fb8c85072dbd296f4295c8a33a5
binhangyuan <yuanbinhang@hotmail.com>
2023-02-27 23:23:34 +0100

    support optiml175

--- start_local_optiml175bmax.sh
+++ start_local_optiml175bmax.sh
@@ -4,4 +4,4 @@ cd /home/fm/dev/Quick_Deployment_HELM
 
 ./together start &
 
-/opt/conda/envs/alpa/bin/python serving_dist_accelerate_nlp_model.py --together_model_name together/galactica-120b --hf_model_name facebook/opt-iml-175b-max --model_path /home/fm/helm_models/hf_models/opt-iml-max
+/opt/conda/envs/alpa/bin/python serving_dist_accelerate_nlp_model.py --together_model_name together/opt-iml-175b-max --hf_model_name facebook/opt-iml-175b-max --model_path /home/fm/helm_models/hf_models/opt-iml-max
