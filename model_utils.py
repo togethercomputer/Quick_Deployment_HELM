b4b4a51f0dad7db7499208bdef2a514e3ea9447b
orangetin <126978607+orangetin@users.noreply.github.com>
2023-06-05 14:03:01 -0700

    Fix auth_token (#26)

--- model_utils.py
+++ model_utils.py
@@ -117,11 +117,11 @@ def get_local_huggingface_tokenizer_model(model_name, model_path=None, dtype=Non
             assert False
     elif model_path is not None and model_path != "":
         logger.warning("model_path is not None, but model_name is not given. Load from model_path only")
-        tokenizer = AutoTokenizer.from_pretrained(model_path, use_auth_token=use_auth_token)
-        model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16, use_auth_token=use_auth_token)
+        tokenizer = AutoTokenizer.from_pretrained(model_path, use_auth_token=auth_token)
+        model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.float16, use_auth_token=auth_token)
     else:
-        tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=use_auth_token)
-        model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=use_auth_token)
+        tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=auth_token)
+        model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=auth_token)
 
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
@@ -133,8 +133,8 @@ def get_local_huggingface_tokenizer_model(model_name, model_path=None, dtype=Non
 def get_local_huggingface_tokenizer_model_llm_int8(model_name, model_path=None, dtype=None, auth_token=None):
     if model_path is None:
         model_path = model_name
-    tokenizer = AutoTokenizer.from_pretrained(model_path, use_auth_token=use_auth_token)
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto', load_in_8bit=True, use_auth_token=use_auth_token)
+    tokenizer = AutoTokenizer.from_pretrained(model_path, use_auth_token=auth_token)
+    model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto', load_in_8bit=True, use_auth_token=auth_token)
 
     if tokenizer.pad_token is None:
         tokenizer.pad_token = tokenizer.eos_token
