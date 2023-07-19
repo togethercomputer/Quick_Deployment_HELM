6ec560516fd4265c8cc8f02cb76a8199857116e0
orangetin <126978607+orangetin@users.noreply.github.com>
2023-07-18 21:28:01 -0700

    Build for llama2

--- serve_chip_20b.sh
+++ serve_chip_20b.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/bin/bash 
 /opt/conda/envs/alpa/bin/python serving_local_nlp_model.py --together_model_name together/chip_20b_instruct_alpha --hf_model_name chip_20B_instruct_alpha --model_path /home/user/.together/models/chip-20b
